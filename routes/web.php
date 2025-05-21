4eec76232d1253873ca3d169b5c19c663746c8e4
lothien <burakyilmza@gmail.com>
2025-05-21 20:59:25 +0300

    refactor: remove unused routes and controllers from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\Frontend\HomeController;
-use App\Http\Controllers\Frontend\NewsController;
-use App\Http\Controllers\Frontend\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('auth')->group(function () {
-    Route::get('/login', [UserController::class,'showLoginForm'])->name('login');
-    Route::post('/login', [UserController::class, 'login']);
-    Route::get('/register', [UserController::class,'showRegisterForm'])->name('register');
-    Route::post('/register', [UserController::class,'register']);
-    Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/', function () {
+    return view('welcome');
 });
